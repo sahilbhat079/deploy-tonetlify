ec0d25b60ea423bcbbdc62506687effde792f44b
sahilbhat079 <sahilgani.sahilgani@gmail.com>
2024-02-17 15:27:48 +0530

    some more

--- src/App.js
+++ src/App.js
@@ -5,33 +5,29 @@ import Newscom from './component/Newscom';
 import {
   BrowserRouter as Router,
   Route,
-  Routes
+  Routes,
+  BrowserRouter
 } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar';
 
 export default function App () {
  const apikey=process.env.REACT_APP_API_NEWS_KEY;
    const [progress,Setprogres]=useState(0);
- 
-
  const setProgress=(progress)=>{
     Setprogres(progress);
   }
-  
 
     return (<>
-    
+    <BrowserRouter basename='/deploy-tonetlify'>
     <LoadingBar
     color='#f11946'
     height= {3}
-    progress={progress}
- 
-    />
-    <Router>
+    progress={progress} />
 
+      <Router>
       <Navbar></Navbar>
       <Routes>
-  <Route exact path="/" element={<Newscom setProgress={setProgress}    apikey={apikey} key="general" country="in" category="general"/>}></Route>
+  <Route exact path="/" element={<Newscom setProgress={setProgress}    apikey={apikey}  key="general" country="in" category="general"/>}></Route>
   <Route exact path="/sports" element={<Newscom setProgress={setProgress}   apikey={apikey}  key="sports" country="in" category="sports"/>}></Route>
   <Route exact path="/health" element={<Newscom setProgress={setProgress}   apikey={apikey}  key="health" country="in" category="health"/>}></Route>
   <Route exact path="/science" element={<Newscom setProgress={setProgress}    apikey={apikey} key="science" country="in" category="science"/>}></Route>
@@ -44,6 +40,7 @@ export default function App () {
       </Routes>
 
     </Router>
+    </BrowserRouter>
     </>
     )
   }
